7e640042212de12db3b99b3297cb8d5765004a84
Abd-allah_El-attar <attar1564@hotmail.com>
2024-10-08 10:14:08 +0300

    Added login functionality to login page

--- login_page_code.js
+++ login_page_code.js
@@ -1,5 +1,35 @@
 "use strict";
 
-let submit_button = document.getElementById("submit_button");
+// TODO: Incorporate username and passwords into database rather than hard-coding them
+let users_dict = {
+    "user": {
+        "password": "pcc",
+        "is_admin": false,
+    },
+    "admin": {
+        "password": "admin",
+        "is_admin": true,
+    },
+}
 
+let loginForm = document.getElementById("login_form");
+let loginButton = document.getElementById("sub_button");
+let loginErrorMsg = document.getElementById("incorrect_info_div");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let username = loginForm.username.value;
+    let password = loginForm.password.value;
+
+    for(let username_key in users_dict){
+        if (username_key == username && users_dict[username_key].password == password) {
+            // Successful login to user or admin dashboard
+            let redirect = (users_dict[username_key].is_admin) ? "./admin_dashboard.html" : "./user_dashboard.html"
+            window.location.href = redirect
+        } else {
+            // Error message shows up when invalid username / password are given
+            loginErrorMsg.style.opacity = 1;
+        }
+    }
+})
 
