2968406d3eba62a75b34c400edb357e5bc66b8a7
Abd-allah_El-attar <attar1564@hotmail.com>
2024-10-10 13:14:50 +0300

    Added JS for summary generation menu

---
+++ menus/generate_summary.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let submit_btn = document.getElementById("submit-btn");
+let cancel_btn = document.getElementById("cancel-btn");
+
+// Cancel button functionality
+cancel_btn.addEventListener('click', (e) => {
+    window.close();
+})
+
+// Submit button functionality
